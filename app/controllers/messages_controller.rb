49adbdf4c829ca7c1ebf28b34cbaf577c230f75d
Ayami Doubuchi <na7.niwa28.galaxyxy@gmail.com>
2016-09-18 12:19:03 +0000

    message-board3

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,26 @@
 # -*- coding: utf-8 -*-
 class MessagesController < ApplicationController
   def index
-   @message = Message.new
-   #messageを全て取得する
-   @messages = Message.all
+    @message = Message.new
+    # Messageを全て取得する。
+    @messages = Message.all
   end
 
   def create
-   @message = Message.new(message_params)
-   @message.save
-   redirect_to root_path , notice: 'メッセージを保存しました'
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to root_path , notice: 'メッセージを保存しました'
+    else 
+      # メッセージが保存できなかった時
+      @message = Message.all
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
+　　  render 'index'
+　  end
   end
 
   private
   def message_params
-    params.require(:message).params(:name, :body)
+    params.require(:message).permit(:name, :body)
   end
-  ##ここまで
+  ## ここまで
 end
